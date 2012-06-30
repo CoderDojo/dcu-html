02ca15e17b99ab05588857cd3946a48fe26b9895
Noel King <noelking@gmail.com>
2012-06-30 22:58:57 +0100

    Animation 001

---
+++ 010 Intro to Animation/animation.js
@@ -0,0 +1,10 @@
+//resizes the image
+function sizeImage() {
+	
+	//get the image object
+	var image = document.getElementById("logo");
+	
+	//increase the image size
+	image.style.width = "1000px";
+	
+}
